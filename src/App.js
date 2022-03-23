fba8d123bf36a353d64207a0e91c72f93b771f1e
ishant005 <91171097+ishant005@users.noreply.github.com>
2022-03-23 16:42:19 +0530

    feat:

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import {React,  useState } from 'react'
- import {CounterContext} from "./CounterContext";
-  import FComponent from "./FComponent";
- function App(){
-   const[counter,setCounter]=useState(0);
+import { React, useState } from "react";
+import { CounterContext } from "./CounterContext";
+import FComponent from "./FComponent";
+function App() {
+  const [counter, setCounter] = useState(1);
 
-   const increment=()=>{
-      setCounter(counter+1);
-   }
-   const decrement=()=>{
-     setCounter(counter-1);
-   }
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+  const decrement = () => {
+    setCounter(counter - 1);
+  };
 
-   return(
-     <div>
+  return (
+    <div>
       <h1>context hook</h1>
       <h1>{counter}</h1>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
-       <CounterContext.Provider value={{counter,setCounter}}>
-       <FComponent/>
-         </CounterContext.Provider>
-       </div>
-   )
- }
+      <CounterContext.Provider value={{ counter, setCounter }}>
+        <FComponent />
+      </CounterContext.Provider>
+    </div>
+  );
+}
 export default App;
